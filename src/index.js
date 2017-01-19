98300572f8d7840a69869e31f14bdc2ac4a4783e
Raphaël Louvradoux <raphael.louvradoux@slate.fr>
2017-01-19 22:31:46 +0100

    Remove words that are less than 3 letters long

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-var countSample = function() {
-  var sample = require('./sample.json');
-  var wordsArray = sample.split(' ');
-  console.log(wordsArray);
-};
-
 module.exports = {
-  countSample: countSample
+  countSample: function() {
+    var sample     = require('./sample.json');
+    var wordsArray = sample.split(' ');
+
+    wordsArray = wordsArray.filter(function(val) { return val.length > 2 });
+
+    // console.log('Array has ' + wordsArray.length + ' elements.');
+    // console.log(wordsArray);
+  }
 };
