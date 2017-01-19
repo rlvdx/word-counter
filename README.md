028e52dddf7aa510e3b4ee754882e051e42dda9a
Raphaël Louvradoux <raphael.louvradoux@slate.fr>
2017-01-19 21:21:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WordCounter lib
+
+> Count the occurrences words with more than two letters in a text
---
+++ src/index.js
@@ -0,0 +1,15 @@
+WordCounter = function() {
+  'use strict';
+
+  function WordCounter (text) {
+
+  }
+
+  var counter = {
+    sayHello: function() {
+      console.log('Hello.');
+    }
+  }
+
+  return counter;
+}();
