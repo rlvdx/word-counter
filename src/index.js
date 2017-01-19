90b355fc1f61203946cc8c888129b202e7a2dae5
Raphaël Louvradoux <raphael.louvradoux@slate.fr>
2017-01-19 23:00:35 +0100

    Add logic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 module.exports = {
   countSample: function() {
-    var sample     = require('./sample.json');
-    var wordsArray = sample.split(' ');
+    var sample = require('./sample.json');
+    // Capitalized or not, a word is considered the same
+    sample = sample.toLowerCase();
+    // Only keep letters for comparison and whitespaces for separation
+    sample = sample.replace(/[^a-z ]/g, '');
 
+    var wordsArray  = sample.split(' ');
+    // Remove words that have strictly less than three letters
     wordsArray = wordsArray.filter(function(val) { return val.length > 2 });
 
-    // console.log('Array has ' + wordsArray.length + ' elements.');
-    // console.log(wordsArray);
+    var wordsObject = {};
+    wordsArray.forEach(function(word) {
+      if (wordsObject.hasOwnProperty(word)) {
+        wordsObject[word]++;
+        return true;
+      }
+
+      wordsObject[word] = 1;
+    });
+
+    console.log(wordsObject);
   }
 };
